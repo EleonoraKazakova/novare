39bfc83498aa9358c0ce6f1ffaa94abf7a7d7a55
Eleonora Kazakova <abei-2005@yandex.ru>
2022-01-08 17:22:17 +0100

    Add checked value

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,32 @@ export default function App() {
     if (item === '') {
       return
     }
-    setItems([...items, [item, price]])
+    setItems([...items, { item: item, price: price, checked: false }])
     setItem('')
   }
 
-  
-  const sortItems = () => { setItems([...items].sort((a, b) => a[0] > b[0] ? 1 : -1)) }
+  // toggleChecked will change checked value to opposite value for element with given index in items
+  const toggleChecked = (index) => {
+    let arr = []
+    for (let i = 0; i < items.length; i++) {
+      if (i === index) {
+        arr.push({ item: items[i]['item'], price: items[i]['price'], checked: !items[i]['checked'] })
+      } else {
+        arr.push(items[i])
+      }
+    }
+    setItems(arr)
+  }
 
-  
-  const sortPrice = () => { setItems([...items].sort((a, b) => a[1] > b[1] ? 1 : -1)) }
+
+
+  const sortItems = () => {
+    setItems([...items].sort((a, b) => a[item] > b[item] ? 1 : -1))
+  }
+
+  const sortPrice = () => {
+    setItems([...items].sort((a, b) => a[price] > b[price] ? 1 : -1))
+  }
   console.log('items:', items)
 
   return (
@@ -30,16 +47,31 @@ export default function App() {
       <div className='app-header'></div>
       <div className='app-content'>
         <HomePage />
+
         {items.length > 0
-          ? <div>
-            <div><input type="checkbox" name='item' onClick={() => sortItems()} /> <label for='item'>Sort by item </label></div>
-            <div><input type="checkbox" name='price' onClick={() => sortPrice()} /> <label for='price'>Sort by price </label></div>
+          ? <div className='app-sort-completed'>
+            <div>
+              <input
+                type='checkbox'
+                name='sort'
+              />
+              <label for='sort'>completed/acquired</label>
+            </div>
+            <div className='app-sort'> Sort by:
+              <div onClick={() => sortItems()} className='app-sort-kind'>  Name </div>
+              <div onClick={() => sortPrice()} className='app-sort-kind'>  Price </div>
+            </div>
           </div>
           : null}
 
-        { items.map(el => <div> <input type="checkbox" /> <label for={el}> {el[0]}, ${el[1]}</label>  </div>) }
+        {items.map((el, index) => <div>
+          <input type="checkbox" checked={el.checked} onChange={() => toggleChecked(index)} />
+          <label for={el}> {el['item']}, ${el['price']}</label>
+        </div>)
+        }
 
-        {/*<div> <input type="checkbox" /> {items.join(', ')} </div>*/}
+
+
 
         {addItem
           ? <ShoppingList
@@ -52,7 +84,7 @@ export default function App() {
           Add item
         </div>
       </div>
-      <div className='app-footer'>footer</div>
+      <div className='app-footer'>Eleonora Kazakova</div>
     </div>
   )
 }
